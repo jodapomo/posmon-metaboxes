6005061402f27605d6a6adf569a49492a87bc7c4
Jose Posada <josedanielpm@gmail.com>
2018-08-01 00:30:35 -0500

    31/07/2018

--- posmon-metaboxes.php
+++ posmon-metaboxes.php
@@ -19,6 +19,7 @@ add_action( 'cmb2_admin_init', 'campos_lineas' );
 function campos_lineas() {
     $prefix = 'posmon_campos_lineas_';
 
+    // METABOX COLOR CONFIGURATION
     $metabox_color = new_cmb2_box( array(
         'id'            => $prefix . 'metabox_color',
         'title'         => __('Configuración de color', 'cmb2'),
@@ -82,66 +83,280 @@ function campos_lineas() {
     ));
 
 
+    // METABOX INDEX VIEW
     $metabox_index = new_cmb2_box( array(
         'id'            => $prefix . 'metabox_index',
-        'title'         => __('Pantalla Principal', 'cmb2'),
+        'title'         => __('Vista en Página Principal', 'cmb2'),
         'object_types'  => array('lineas'),
     ));
 
+    $metabox_index->add_field( array(
+        'name'    => 'Fondo',
+        'desc'    => 'Imagen cuadrada de fondo para la página principal.',
+        'id'      => $prefix . 'index_bg_image',
+        'type'    => 'file',
+        // Optional:
+        'options' => array(
+            'url' => false, // Hide the text input for the url
+        ),
+        'text'    => array(
+            'add_upload_file_text' => 'Subir Imagen' // Change upload button text. Default: "Add or Upload File"
+        ),
+        // query_args are passed to wp.media's library query.
+        'query_args' => array(
+            'type' => array(
+            	'image/gif',
+            	'image/jpeg',
+            	'image/png',
+            ),
+        ),
+        'preview_size' => 'large', // Image size to use when previewing in the admin.
+    ) );
+
+    $metabox_index->add_field( array(
+        'name'    => 'Imagen Modelo',
+        'desc'    => 'Imagen con modelo del cuadrado en la página principal.',
+        'id'      => $prefix . 'index_modelo_image',
+        'type'    => 'file',
+        // Optional:
+        'options' => array(
+            'url' => false, // Hide the text input for the url
+        ),
+        'text'    => array(
+            'add_upload_file_text' => 'Subir Imagen' // Change upload button text. Default: "Add or Upload File"
+        ),
+        // query_args are passed to wp.media's library query.
+        'query_args' => array(
+            'type' => array(
+            	'image/gif',
+            	'image/jpeg',
+            	'image/png',
+            ),
+        ),
+        'preview_size' => 'large', // Image size to use when previewing in the admin.
+    ) );
+
+
+    // METABOX MAIN SLIDER
     $metabox_slider_principal = new_cmb2_box( array(
         'id'            => $prefix . 'metabox_slider_principal',
         'title'         => __('Slider Principal', 'cmb2'),
         'object_types'  => array('lineas'),
     ));
 
+    $metabox_slider_principal->add_field( array(
+        'name'      => __('Color Título', 'cmb2'),
+        'id'      => $prefix . 'color_titulo_slider_principal',
+        'type'    => 'colorpicker',
+        'default' => '#ffffff',
+        'attributes' => array(
+            'data-colorpicker' => json_encode( array(
+                // Iris Options set here as values in the 'data-colorpicker' array
+                'palettes' => array( '#000', '#fff'),
+            ) ),
+        ),
+    ));
+
+    $metabox_slider_principal->add_field( array(
+        'name' => 'Descripción',
+        'id' => $prefix . 'slider_principal_desc',
+        'type' => 'textarea_small'
+    ) );
+
+    $metabox_slider_principal->add_field( array(
+        'name'      => __('Color Descripción', 'cmb2'),
+        'id'      => $prefix . 'color_desc_slider_principal',
+        'type'    => 'colorpicker',
+        'default' => '#ffffff',
+        'attributes' => array(
+            'data-colorpicker' => json_encode( array(
+                // Iris Options set here as values in the 'data-colorpicker' array
+                'palettes' => array( '#000', '#fff'),
+            ) ),
+        ),
+    ));
+
+    $metabox_slider_principal->add_field( array(
+        'name'    => 'Imagen Modelo',
+        'desc'    => 'Imagen con modelo del slider principal de la línea.',
+        'id'      => $prefix . 'slider_principal_modelo_image',
+        'type'    => 'file',
+        // Optional:
+        'options' => array(
+            'url' => false, // Hide the text input for the url
+        ),
+        'text'    => array(
+            'add_upload_file_text' => 'Subir Imagen' // Change upload button text. Default: "Add or Upload File"
+        ),
+        // query_args are passed to wp.media's library query.
+        'query_args' => array(
+            'type' => array(
+            	'image/gif',
+            	'image/jpeg',
+            	'image/png',
+            ),
+        ),
+        'preview_size' => 'large', // Image size to use when previewing in the admin.
+    ));
+
+    $metabox_slider_principal->add_field( array(
+        'name'    => 'Imagen Fondo Difuminada',
+        'desc'    => 'Imagen difuminada para el fondo del slider principal.',
+        'id'      => $prefix . 'slider_principal_fondo_image',
+        'type'    => 'file',
+        // Optional:
+        'options' => array(
+            'url' => false, // Hide the text input for the url
+        ),
+        'text'    => array(
+            'add_upload_file_text' => 'Subir Imagen' // Change upload button text. Default: "Add or Upload File"
+        ),
+        // query_args are passed to wp.media's library query.
+        'query_args' => array(
+            'type' => array(
+            	'image/gif',
+            	'image/jpeg',
+            	'image/png',
+            ),
+        ),
+        'preview_size' => 'large', // Image size to use when previewing in the admin.
+    ));
+
+    // METABOX SLIDERS
     $metabox_sliders = new_cmb2_box( array(
         'id'            => $prefix . 'metabox_sliders',
         'title'         => __('Sliders', 'cmb2'),
         'object_types'  => array('lineas'),
     ));
 
-
     $metabox_sliders->add_field( array(
-        'id'          => 'wiki_test_repeat_group',
+        'id'          => $prefix . 'sliders_group',
         'type'        => 'group',
-        'description' => __( 'Generates reusable form entries', 'cmb2' ),
+        'description' => '<img src="' . get_template_directory_uri() . '/img/slider_guide_type.png' . '" style="width: 100%;">',
         'object_types'  => array('lineas'),
         'options'     => array(
-            'group_title'   => __( 'Entry {#}', 'cmb2' ), // since version 1.1.4, {#} gets replaced by row number
-            'add_button'    => __( 'Add Another Entry', 'cmb2' ),
-            'remove_button' => __( 'Remove Entry', 'cmb2' ),
+            'group_title'   => __( 'Slider {#}', 'cmb2' ), // since version 1.1.4, {#} gets replaced by row number
+            'add_button'    => __( 'Agregar otro Slider', 'cmb2' ),
+            'remove_button' => __( 'Eliminar Slider', 'cmb2' ),
             'sortable'      => true, // beta
-            'closed'     => true, // true to have the groups closed by default
+            // 'closed'     => true, // true to have the groups closed by default
         ),
     ) );
 
 
     // Id's for group's fields only need to be unique for the group. Prefix is not needed.
-    $metabox_sliders->add_group_field('wiki_test_repeat_group', array(
-    'name' => 'Entry Title',
-    'id'   => 'title',
-    'type' => 'text',
-    // 'repeatable' => true, // Repeatable fields are supported w/in repeatable groups (for most types)
-    ) );
+    $metabox_sliders->add_group_field($prefix . 'sliders_group', array(
+        'name'    => 'Tipo',
+        'id'      => 'type',
+        'type'    => 'radio_inline',
+        'options' => array(
+            'blur-bg'   => __( 'Tipo 1', 'cmb2' ),
+            'full-bg'   => __( 'Tipo 2', 'cmb2' ),
+            'half-img'  => __( 'Tipo 3', 'cmb2' ),
+        ),
+        'default' => 'blur-bg',
+    ));
 
-    $metabox_sliders->add_group_field('wiki_test_repeat_group', array(
-    'name' => 'Description',
-    'description' => 'Write a short description for this entry',
-    'id'   => 'description',
-    'type' => 'textarea_small',
-    ) );
+    $metabox_sliders->add_group_field($prefix . 'sliders_group', array(
+        'name'    => 'Tipo',
+        'id'      => 'type',
+        'type'    => 'radio_inline',
+        'options' => array(
+            'blur-bg'   => __( 'Tipo 1', 'cmb2' ),
+            'full-bg'   => __( 'Tipo 2', 'cmb2' ),
+            'half-img'  => __( 'Tipo 3', 'cmb2' ),
+        ),
+        'default' => 'blur-bg',
+    ));
 
-    $metabox_sliders->add_group_field('wiki_test_repeat_group', array(
-    'name' => 'Entry Image',
-    'id'   => 'image',
-    'type' => 'file',
-    ) );
 
-    $metabox_sliders->add_group_field('wiki_test_repeat_group', array(
-    'name' => 'Image Caption',
-    'id'   => 'image_caption',
-    'type' => 'text',
-    ) );
+
+
+    $metabox_sliders->add_group_field($prefix . 'sliders_group', array(
+        'name' => 'Título',
+        'id'   => 'title',
+        'type' => 'text',
+    ));
+
+    $metabox_sliders->add_group_field($prefix . 'sliders_group', array(
+        'name'      => __('Color Título', 'cmb2'),
+        'id'      => 'colot_title',
+        'type'    => 'colorpicker',
+        'default' => '#ffffff',
+        'attributes' => array(
+            'data-colorpicker' => json_encode( array(
+                // Iris Options set here as values in the 'data-colorpicker' array
+                'palettes' => array( '#000', '#fff'),
+            ) ),
+        ),
+    ));
+
+    $metabox_sliders->add_group_field($prefix . 'sliders_group', array(
+        'name' => 'Descripción',
+        'id'   => 'desc',
+        'type' => 'textarea_small',
+    ));
+
+    $metabox_sliders->add_group_field($prefix . 'sliders_group', array(
+        'name'      => __('Color Descripción', 'cmb2'),
+        'id'      => 'color_desc',
+        'type'    => 'colorpicker',
+        'default' => '#ffffff',
+        'attributes' => array(
+            'data-colorpicker' => json_encode( array(
+                // Iris Options set here as values in the 'data-colorpicker' array
+                'palettes' => array( '#000', '#fff'),
+            ) ),
+        ),
+    ));
+
+
+    $metabox_sliders->add_group_field($prefix . 'sliders_group', array(
+        'name'    => 'Imagen',
+        'desc'    => '<h3>Según el tipo de slider:</h3>- Tipo 1: imagen fondo slider (se recomienda sea desenfocada). <br> - Tipo 2: imagen fondo slider (imagen mucho más ancha que alta). <br> - Tipo 3: imagen a la derecha del slider.',
+        'id'      => 'image',
+        'type'    => 'file',
+        // Optional:
+        'options' => array(
+            'url' => false, // Hide the text input for the url
+        ),
+        'text'    => array(
+            'add_upload_file_text' => 'Subir Imagen' // Change upload button text. Default: "Add or Upload File"
+        ),
+        // query_args are passed to wp.media's library query.
+        'query_args' => array(
+            'type' => array(
+            	'image/gif',
+            	'image/jpeg',
+            	'image/png',
+            ),
+        ),
+        'preview_size' => 'large', // Image size to use when previewing in the admin.
+    ));
+
+    $metabox_sliders->add_group_field($prefix . 'sliders_group', array(
+        'name'    => 'Imagen Modelo',
+        'desc'    => 'Imagen con modelo. Sólo aplica para slider Tipo 1',
+        'id'      => 'image_modelo',
+        'type'    => 'file',
+        // Optional:
+        'options' => array(
+            'url' => false, // Hide the text input for the url
+        ),
+        'text'    => array(
+            'add_upload_file_text' => 'Subir Imagen' // Change upload button text. Default: "Add or Upload File"
+        ),
+        // query_args are passed to wp.media's library query.
+        'query_args' => array(
+            'type' => array(
+            	'image/gif',
+            	'image/jpeg',
+            	'image/png',
+            ),
+        ),
+        'preview_size' => 'large', // Image size to use when previewing in the admin.
+    ));
+
 
 }
 
